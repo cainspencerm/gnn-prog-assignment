de3e8b9caafb6903e487db837511f1e010c46cd0
TrinaDutta95 <77251570+TrinaDutta95@users.noreply.github.com>
2022-07-14 15:18:01 -0400

    Spectral and spatial are not yet tested

--- models/spatial.py
+++ models/spatial.py
@@ -1,17 +1,28 @@
 from torch import nn
+import torch.nn.functional as F
+import dgl
+from dgl.nn import GATConv
 
 
 class Classifier(nn.Module):
     def __init__(self):
         super().__init__()
-
-
-
-        # Determined in parameter search.
+        self.gat1 = GATConv(256, 64, heads=3)
+        self.gat2 = GATConv(64, 10, heads=3)
+        self.optimizer = nn.optim.Adam(self.parameters(),
+                                          lr=0.005,
+                                          weight_decay=5e-4)
+       
+       # Determined in parameter search.
         self.defaults = {'epochs': 20, 'batch_size': 32, 'learning_rate': 5e-3}
 
-    def forward(self, x, training=True):
-        return x
+    def forward(self, x, edge_index, training=True):
+        h = F.dropout(x, p=0.6, training=self.training)
+        h = self.gat1(x, edge_index)
+        h = F.elu(h)
+        h = F.dropout(h, p=0.6, training=self.training)
+        h = self.gat2(h, edge_index)
+        return h, F.log_softmax(h, dim=1)
 
     def get_defaults(self):
         return 'epochs_' + str(self.defaults['epochs']) + \
